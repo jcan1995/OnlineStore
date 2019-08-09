c873a121f3939f7a82ee0f4b708538033af19836
Robbie08 <roberinho08@gmail.com>
2019-08-08 21:06:27 -0700

    Added item images to folder

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,8 +2,9 @@ import React from "react";
 
 const HomePage = (prop) => {
 
-    console.log(prop); // to see what we are recieving
+    console.log(prop); // to see what we are recieving form the prop
 
+    /* Conditional Render for un/authenticated users */
     if(prop.auth){
         return <DisplayAuth name={prop.name}/>
     }
@@ -18,13 +19,10 @@ const HomePage = (prop) => {
         - User should be prompted to sign up to recieve discount
 */
 const DisplayUnauth = () => {
-
-
     return(
         <div class="body">
             <h1>Welcome, Sign up to recieve 20% Off todays purchase!</h1>
         </div>
-
     )
 }
 
@@ -33,14 +31,12 @@ const DisplayUnauth = () => {
         - User should be prompted to invite friends to recieve discount
 */
 const DisplayAuth = (data) => {
-
-
+    console.log(data.name); // to check what we are receiving from data.name
     return(
         <div class="body">
             <h1>Hello, {data.name}</h1>
             <p>Send your friends an invite to join and we will give you 15% Off when they sign up!</p>
         </div>
-
     )
 }
 
